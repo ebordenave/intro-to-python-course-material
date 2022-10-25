60b92cd7557512e6e434d3fb1de22dd757fd9bdf
ebordenave <10405296+ebordenave@users.noreply.github.com>
2022-10-25 15:10:52 -0400

    Merge remote-tracking branch 'origin/main'

--- 02_functions/homework.py
+++ 02_functions/homework.py
@@ -42,7 +42,7 @@ def productSum(x: int, y: int, z: int) -> int:
         return sum_xy
 
 
-def resultOverrider(x: int, y: int, op: str = '+', override: callable = None) -> int:
+def resultOverrider(x: int, y: int, op: str = '+', override: callable = None) -> float:
     """Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
      magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
@@ -61,6 +61,6 @@ def resultOverrider(x: int, y: int, op: str = '+', override: callable = None) ->
         elif op == '*':
             return x * y
         elif op == '/':
-            return x // y
+            return x / y
 
 
