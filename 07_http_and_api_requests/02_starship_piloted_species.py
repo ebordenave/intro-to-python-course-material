8b8a7d182259b6f68acdbf753c9ae708fc1679f7
ebordenave <10405296+ebordenave@users.noreply.github.com>
2022-11-25 09:51:58 -0500

    added pilot species search function and wear a jacket search function

--- 07_http_and_api_requests/02_starship_piloted_species.py
+++ 07_http_and_api_requests/02_starship_piloted_species.py
@@ -1,33 +1,6 @@
 import requests
 
 
-def sw_vehicle_search(cargo_capacity: int, max_speed: int, cost: int) -> list:
-    all_vehicles = []
-    query_matches = []
-    url = 'https://swapi.dev/api/vehicles/'
-    response = requests.get(url)
-    data = response.json()
-
-    # store the first page of results
-    all_vehicles = all_vehicles + data['results']
-
-    while data['next'] is not None:
-        print("Next page found, downloading", data['next'])
-        response = requests.get(data['next'])
-        data = response.json()
-        all_vehicles = all_vehicles + data['results']
-
-    for vehicles in all_vehicles:
-        try:
-            if int(vehicles['cargo_capacity']) >= cargo_capacity and int(
-                    vehicles['max_atmosphering_speed']) >= max_speed and int(vehicles['cost_in_credits']) <= cost:
-                query_matches.append(vehicles['name'])
-        except ValueError:
-            pass
-    # print(f"vehicles that match query => {query_matches}")
-    return query_matches
-
-
 def starship_piloted_species(starship: str) -> list:
     total_starships = []
     query_matches = []
@@ -61,3 +34,6 @@ def starship_piloted_species(starship: str) -> list:
                         # print(species)
     print(query_matches)
     return query_matches
+
+
+starship_piloted_species('Millennium Falcon')
