ccaf3f6de924fc8caa52f784f83ed765a4af050e
ebordenave <10405296+ebordenave@users.noreply.github.com>
2022-11-27 01:01:45 -0500

    added borders function

--- 07_http_and_api_requests/05_border.py
+++ 07_http_and_api_requests/05_border.py
@@ -3,11 +3,34 @@ import json
 
 
 # def borders(country_name_a: str, country_name_b: str) -> bool:
-def borders(country_name_a: str) -> bool:
+def borders(country_name_a: str, country_name_b: str) -> bool:
     url = 'https://restcountries.com/v3.1/name/'
-    response = requests.get(url + country_name_a)
-    data = response.json()
-    print(data)
+    a_response = requests.get(url + country_name_a)
+    b_response = requests.get(url + country_name_b)
+
+    a_data = a_response.json()
+    b_data = b_response.json()
+
+    for item in a_data:
+        try:
+            print(
+                f"country A name with '{country_name_a}' in it => {item['name']['common']} and it borders {item['borders']}")
+        except KeyError:
+            pass
+    print('-' * 150)
+    for item in b_data:
+        try:
+            print(
+                f"country B name with '{country_name_b}' in it => {item['name']['common']} and it borders {item['borders']}")
+        except KeyError:
+            pass
 
 
-borders('united')
+        # print(f"dictionary => {item}")
+
+    # print(data)
+    # for item in data:
+    #     print(item)
+
+
+borders('united', 'republic')
